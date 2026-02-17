a27b083cd460edd7cd5e4adfba60892de6477bc8
Casey Link <casey@outskirtslabs.com>
2026-02-17 12:17:40 +0100

    clean gulp tasks

--- ui/gulpfile.js
+++ ui/gulpfile.js
@@ -1,22 +1,17 @@
 'use strict'
 
-const { parallel, series, watch } = require('gulp')
+const { parallel, series } = require('gulp')
 const createTask = require('./gulp.d/lib/create-task')
 const exportTasks = require('./gulp.d/lib/export-tasks')
 const log = require('fancy-log')
 
 const bundleName = 'ui'
 const buildDir = 'build'
-const previewSrcDir = 'preview-src'
-const previewDestDir = 'public'
 const srcDir = 'src'
-const destDir = `${previewDestDir}/_`
-const { reload: livereload } = process.env.LIVERELOAD === 'true' ? require('gulp-connect') : {}
-const serverConfig = { host: '0.0.0.0', port: 5252, livereload }
+const stageDir = 'public/_'
 
 const task = require('./gulp.d/tasks')
 const glob = {
-  all: [srcDir, previewSrcDir],
   css: `${srcDir}/css/**/*.css`,
   js: ['gulpfile.js', 'gulp.d/**/*.js', `${srcDir}/{helpers,js}/**/*.js`],
   test: ['test/**/*.js'],
@@ -73,11 +68,7 @@ const formatTask = createTask({
 const buildTask = createTask({
   name: 'build',
   desc: 'Build and stage the UI assets for bundling',
-  call: task.build(
-    srcDir,
-    destDir,
-    process.argv.slice(2).some((name) => name.startsWith('preview'))
-  ),
+  call: task.build(srcDir, stageDir),
 })
 
 const bundleBuildTask = createTask({
@@ -89,7 +80,7 @@ const bundlePackTask = createTask({
   name: 'bundle:pack',
   desc: 'Create a bundle of the staged UI assets for publishing',
   call: task.pack(
-    destDir,
+    stageDir,
     buildDir,
     bundleName,
     (bundlePath) => !process.env.CI && log(`Antora option: --ui-bundle-url=${bundlePath}`)
@@ -108,37 +99,12 @@ const packTask = createTask({
   call: series(bundleTask),
 })
 
-const buildPreviewPagesTask = createTask({
-  name: 'preview:build-pages',
-  call: task.buildPreviewPages(srcDir, previewSrcDir, previewDestDir, livereload),
-})
-
-const previewBuildTask = createTask({
-  name: 'preview:build',
-  desc: 'Process and stage the UI assets and generate pages for the preview',
-  call: parallel(buildTask, buildPreviewPagesTask),
-})
-
-const previewServeTask = createTask({
-  name: 'preview:serve',
-  call: task.serve(previewDestDir, serverConfig, () => watch(glob.all, previewBuildTask)),
-})
-
-const previewTask = createTask({
-  name: 'preview',
-  desc: 'Generate a preview site and launch a server to view it',
-  call: series(previewBuildTask, previewServeTask),
-})
-
 module.exports = exportTasks(
   bundleTask,
   cleanTask,
   lintTask,
   formatTask,
   buildTask,
-  bundleTask,
   bundlePackTask,
-  previewTask,
-  previewBuildTask,
   packTask
 )
