19a7477251db7e5c6fc0534fedb530a3169d6f69
Casey Link <casey@outskirtslabs.com>
2026-02-17 13:03:41 +0100

    fix breadcrumbs

--- ui/src/helpers/versioned_url.js
+++ ui/src/helpers/versioned_url.js
@@ -4,9 +4,14 @@ module.exports = (siteUrl, versionSegment, url) => {
   if (!url) {
     // occurs with stock pages like 404.html
     return url
-  } else if (url.startsWith(`/${versionSegment}/`)) {
-    return `${siteUrl}${url}`
-  } else {
-    return `${siteUrl}/${versionSegment}${url}`
   }
+
+  const normalizedUrl = url.startsWith('/') ? url : `/${url}`
+  const versionPath = versionSegment && !normalizedUrl.includes(`/${versionSegment}/`)
+    ? `/${versionSegment}${normalizedUrl}`
+    : normalizedUrl
+
+  if (!siteUrl) return versionPath
+
+  return `${siteUrl.replace(/\/+$/, '')}${versionPath}`
 }
