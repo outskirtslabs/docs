e84271528f20ca42f6b1dd8e88d4ce2ddbbec85f
Casey Link <casey@outskirtslabs.com>
2026-09-25 12:11:07 +0200

    docs: include protocol53 release documentation

--- pkgs/projects.nix
+++ pkgs/projects.nix
@@ -113,7 +113,7 @@
     rev = "73f96caae06522a5e7eedf0cc74882f20d67d0a4";
     hash = "sha256-KaOfotIGy7ovYICV4DSPBiDdDwUncD170fZ2Q1dpWIQ=";
     refs_hash = "sha256-4lDHVHemP/Bde7BB+tvU2KL1VvCAjM0cOiryoSbpGa8=";
-    branches = [ "HEAD" ];
+    branches = [ "HEAD" "docs/v*" ];
     start_path = "doc";
   };
 
